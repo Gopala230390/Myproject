d3e721cc21009335c70b70c6823594530802b865
gopala.krishnan <valid.gopalakrishnan.2390@gmail.com>
2023-06-28 04:33:39 +0530

    rr

---
+++ src/Star/Myproject.java
@@ -0,0 +1,25 @@
+package Star;
+
+public class Myproject {
+	
+	public int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisor cannot be zero");
+        }
+        return (double) a / b;
+    }
+	
+
+}
